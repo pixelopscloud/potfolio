aaa75b138379eb5d32c4e3b84bff4ab88f7c3908
muhammadali <muhammadali@gmail.com>
2025-11-07 17:21:11 -0500

    change welcome file

--- Welcome.java
+++ Welcome.java
@@ -1,8 +1,25 @@
+cat > Welcome.java << 'EOF'
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Welcome {
-    public static void main(String[] args) throws InterruptedException {
-        while(true) {
-            System.out.println("Welcome to Java + Jenkins + Docker pipeline!");
-            Thread.sleep(5000); // Wait 5 seconds between prints
-        }
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
+        server.createContext("/", new HttpHandler() {
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "<html><body><h1>Welcome to Java + Jenkins + Docker pipeline!</h1></body></html>";
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+        server.start();
+        System.out.println("Server started on port 80");
     }
 }
+EOF
